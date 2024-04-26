3d543df5808e578d5cdd3d1a3e5754214f8f9e16
dcatwoohoo <57969474+dcatwoohoo@users.noreply.github.com>
2024-04-25 20:08:59 -0500

    Update chatbot.py
    Enable logic adapters for math and time

--- app/chatbot.py
+++ app/chatbot.py
@@ -1,3 +1,11 @@
 from chatterbot import ChatBot
 
-chatbot = ChatBot("Sure, Not", read_only=True)
+chatbot = ChatBot(
+    "Sure, Not",
+    logic_adapters=[
+        'chatterbot.logic.BestMatch',
+        'chatterbot.logic.MathematicalEvaluation',
+        'chatterbot.logic.TimeLogicAdapter'
+    ],
+    read_only=True
+)
